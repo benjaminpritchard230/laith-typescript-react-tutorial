bd40244fe2ce2e9e55480f7bc86b8fc43d7df523
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-01-04 18:04:39 +0000

    Learn typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import List from "./components/List";
+import AddToList from "./components/AddToList";
 
-interface IState {
+export interface IState {
   people: {
     name: string;
     age: number;
-    url: string;
-    note: string;
+    img: string;
+    note?: string;
   }[];
 }
 
@@ -16,22 +17,23 @@ function App() {
   const [people, setPeople] = useState<IState["people"]>([
     {
       name: "Lebron James",
-      url: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQVl1muj07W9miwgXG7APQflVip1y6q1o9H12NbHyCIAcEnl0SBn5Fk9_zrxPLY7y_P8WG5p9ld17MTU0s",
+      img: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQVl1muj07W9miwgXG7APQflVip1y6q1o9H12NbHyCIAcEnl0SBn5Fk9_zrxPLY7y_P8WG5p9ld17MTU0s",
       age: 36,
       note: "tall",
     },
     {
       name: "Kobe",
-      url: "https://www.biography.com/.image/t_share/MTgwMzQyOTk2OTY3ODkyODQ0/gettyimages-490703338.jpg",
+      img: "https://www.biography.com/.image/t_share/MTgwMzQyOTk2OTY3ODkyODQ0/gettyimages-490703338.jpg",
       age: 36,
       note: "",
     },
   ]);
 
   return (
-    <div className="App">
+    <div className="AddToList">
       <h1>People invited to my party</h1>
       <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,22 +1,18 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { IState as Props } from "../App";
 
-interface Props {
-  people: {
-    name: string;
-    age: number;
-    url: string;
-    note: string;
-  }[];
+interface IProps {
+  people: Props["people"];
 }
 
-const List = ({ people }: Props) => {
+const List: React.FC<IProps> = ({ people }) => {
   const renderList = (): JSX.Element[] => {
     return people.map((person) => {
       return (
         <li className="List">
           <div className="List-header">
-            <img className="List-img" src={person.url} alt="" />
+            <img className="List-img" src={person.img} alt="" />
             <h2>{person.name}</h2>
             <p>{person.age} years old</p>
             <p className="List-note">{person.note}</p>
